2f9c7c33458533029939eaefe379c47e6e7b3c3e
Vladyslav Horban <ywvvvw@gmail.com>
2023-07-25 19:46:46 +0300

    Add RefreshDataWorker

--- app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
@@ -2,12 +2,10 @@ package com.example.cryptoapp.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cryptoapp.data.repository.CoinRepositoryImpl
 import com.example.cryptoapp.domain.GetCoinInfoListUseCase
 import com.example.cryptoapp.domain.GetCoinInfoUseCase
 import com.example.cryptoapp.domain.LoadDataUseCase
-import kotlinx.coroutines.launch
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -21,8 +19,6 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        viewModelScope.launch {
-            loadDataUseCase()
-        }
+        loadDataUseCase()
     }
 }
